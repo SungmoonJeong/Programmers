2b0f7ad5ec53bc381467bb81d638b00c55c22221
정성문 <140133940+SungmoonJeong@users.noreply.github.com>
2025-01-05 00:32:22 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.29 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -0,0 +1,6 @@
+def solution(my_string, queries):
+    my_string = list(my_string)
+    for i, j in queries:
+        my_string[i:j + 1] = reversed(my_string[i:j + 1])
+
+    return ''.join(my_string)
